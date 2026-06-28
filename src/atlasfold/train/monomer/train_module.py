30718752b06bdf9c15293e33d1c070eb33ee6469
SeonghwanSeo <shwan0106@gmail.com>
2026-06-28 14:51:09 +0900

    update training - use afm disordered pdb

--- src/atlasfold/train/monomer/train_module.py
+++ src/atlasfold/train/monomer/train_module.py
@@ -658,7 +658,7 @@ class TrainingModule(pl.LightningModule):
         checkpoint["ema"] = ema_state_dict
 
     def on_load_checkpoint(self, checkpoint: dict[str, Any]) -> None:
-        self.load_ema_state_dict(checkpoint["ema"], strict=False)
+        self.load_ema_state_dict(checkpoint["ema"], strict=True)
 
     def load_state_dict(
         self, state_dict: Mapping[str, Any], strict: bool = True, assign: bool = False
@@ -672,7 +672,7 @@ class TrainingModule(pl.LightningModule):
             provided_keys = set(state_dict.keys())
             missing_keys = model_keys - provided_keys
             unexpected_keys = provided_keys - model_keys
-            allowed_missing_prefixes = ("lm.", "confidence_head.")
+            allowed_missing_prefixes = ("lm.",)
             actual_missing_keys = [
                 k for k in missing_keys if not k.startswith(allowed_missing_prefixes)
             ]
