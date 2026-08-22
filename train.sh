ee7d5fa0887bb824d570978e54a7c40c101d2f6f
SeonghwanSeo <shwan0106@gmail.com>
2026-08-22 10:37:01 +0900

    remove debug code

--- train.sh
+++
@@ -1,8 +0,0 @@
-python ./scripts/train_monomer_ipa.py \
-  configs/monomer_ipa/train_stage1.yaml \
-  --init_weight weights/atlasfold-stage1.pth \
-  --experiment_name 'v2-stage1-256' \
-  --out_dir '/mnt/parallel_storage/wykim_lab/icl_shwan/entire-train-log/monomer-ipa/' \
-  --wandb \
-  --compile \
-  --override train.data.batch_size=4
